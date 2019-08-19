9f517ae12bdb3b57dd43dbea0fcf22999a9fd0ac
Naga9721 <52663961+Naga9721@users.noreply.github.com>
2019-08-19 12:30:16 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,7 +4,24 @@ jQuery(document).ready(function(){
     btnclick(1);
    
 });
-
+$(document).on('click', '.delete', function () {
+    $("#myModal").appendTo('body');
+    $("#myModal").modal("show");
+    $(document).on('click','#delete',function(){
+        $(".delete").closest('tr').remove();
+        return false;
+        })
+        $(document).on('click','#cancel',function(){
+            $("#myModal").modal("hide");
+        })
+  });
+ $(document).on("click", ".edit", function(){		
+    $(this).parents("tr").find("td:not(:last-child)").each(function(){
+        $(this).html('<input type="text" class="form-control" value="' + $(this).text() + '">');
+    });		
+    $(this).parents("tr").find(".add, .edit").toggle();
+    $(".add-new").attr("disabled", "disabled");
+});
 $(function(){
     $("#sub").click(function(){
     var fname=$("#f").val();
@@ -27,7 +44,7 @@ $(function(){
             console.log(response);
         }
     });
-    var mark="<tr><td style='text-align:center'>"+fname+" "+lname+"</td><td style='text-align:center'>"+mail+"</td><td style='text-align:center'>"+" "+"</td><td  style='text-align:center' > <a class='edit' title='Edit' data-toggle='tooltip'><i style='color:green' class='material-icons'>&#xE254;</a> <a  title='Delete' data-toggle='tooltip'><i id='d'style='color:red' class='material-icon'>&#xE872;</i></a></td></tr>";
+    var mark="<tr><td style='text-align:center'>"+fname+" "+lname+"</td><td style='text-align:center'>"+mail+"</td><td style='text-align:center'>"+" "+"</td><td  style='text-align:center' > <a class='edit' title='Edit' data-toggle='tooltip'><i style='color:green' class='material-icons'>&#xE254;</a> <a  title='Delete' data-toggle='tooltip'><i id='d'style='color:red' class='delete' class='material-icon'>&#xE872;</i></a></td></tr>";
     $("table tbody").append(mark);
 }
 });
@@ -61,7 +78,7 @@ jQuery.ajax({
     var trArr = new Array();
     $.each(response.data, function(i, v){
         trArr.push('<tr><td style="text-align:center">' + v.first_name+" "+v.last_name +'</td><td style="text-align:center" >' + v.email + '</td><td style="text-align:center"><img id="img" src="' 
-        + v.avatar + '" width="100px" /></td><td style="text-align:center"> <a class="edit" title="Edit" data-toggle="tooltip"><i style="color:green" class="material-icons">&#xE254;</a> <a  title="Delete" data-toggle="tooltip"><i id="d" style="color:red" class="material-icons">&#xE872;</i></a>' + '</td></tr>'  );
+        + v.avatar + '" width="100px" /></td><td style="text-align:center"> <a class="edit" title="Edit" data-toggle="tooltip"><i style="color:green" class="material-icons">&#xE254;</a> <a  title="Delete" data-toggle="tooltip"><i id="d" style="color:red" class="delete" class="material-icons">&#xE872;</i></a>' + '</td></tr>'  );
         
     });
     $('table#usertable tbody').append(trArr.join('\n')); 
